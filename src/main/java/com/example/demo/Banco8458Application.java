d5b279273398d26cbb4946b43bab7fcf13ff576c
NEON <mgutie33@hotmail.com>
2022-11-01 09:32:28 -0500

    first commit

---
+++ src/main/java/com/example/demo/Banco8458Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Banco8458Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Banco8458Application.class, args);
+	}
+
+}
